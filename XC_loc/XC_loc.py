fa8e3d1d16f0078a4026527372d19efaa446b810
Aaron Wech <awech@usgs.gov>
2018-04-30 15:58:05 -0800

    error chagning st_cluster to cluster

--- XC_loc/XC_loc.py
+++ XC_loc/XC_loc.py
@@ -626,7 +626,7 @@ class event_list(object):
 
 	def cluster(self,dx=10,dt=120,num_events=4):
 		det=detections(self.events)
-		det.st_cluster(dx=10,dt=120,num_events=4)
+		det.cluster(dx=10,dt=120,num_events=4)
 		return det
 
 # def cluster_spacetime(self,dx=10,dt=120,num_events=4):
