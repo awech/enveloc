f47c3cb5619085aec73333ffa16a8a8dffbe7698
Aaron Wech <awech@usgs.gov>
2020-05-22 15:59:18 -0800

    including .tvel file and new version number

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ using envelope cross correlation and a grid search
 setup(
   name = 'XC_loc',
   packages = find_packages(),
-  package_data={'': ['default_vel_model.tvel']},
+  package_data={'': ['XC_loc/default_vel_model.tvel']},
   include_package_data=True,
-  version = '1.0',
+  version = '1.0.1',
   license='LGPL',
   description = 'XC_loc - envelope cross-correlation location method',   # Give a short description about your library
   long_description = long_description,
   author = 'Aaron Wech',
   author_email = 'awech@usgs.gov',
   url = 'https://github.com/awech/XC_loc',
-  download_url = 'https://github.com/awech/XC_loc/archive/v1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/awech/XC_loc/archive/v1.0.1.tar.gz',    # I explain this later on
   keywords = ['envelope cross correlation', 'seimsology', 'tremor'],
   install_requires=[
           'obspy',
